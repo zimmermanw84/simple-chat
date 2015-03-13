a99a75b8aaef2cff83e02bd336731fb11721741f
Walter Zimmerman <zimmermanw84@gmail.com>
2015-03-13 08:12:27 -0700

    minor spelling change

--- models.js
+++ models.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-module.exports.User= mongoose.model('User', new Schema({
+module.exports.User = mongoose.model('User', new Schema({
 
   username: { type: String, required: true , unique: true },
   password: { type: String, required: true },
